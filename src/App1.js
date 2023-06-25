c09f9c105131134487b916613c016bb7b6cc1ee4
geetanjalising <geetanjalisingh1815@gmail.com>
2023-06-25 12:22:40 +0530

    changed

--- src/App1.js
+++ src/App1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from "./App";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Start from "./Components/Start/Start";
 const App1 = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import App1 from "./App1";
 
 
@@ -9,12 +8,6 @@ import App1 from "./App1";
 ReactDOM.render(
   <React.StrictMode> 
    <App1/>
-    
-    {/* <Route exact path="/" element={<Start/>} /> */}
-  
-    
-     
-    
   </React.StrictMode>,
   document.getElementById("root")
 );
